80acb2f0349bf22706b0c0a60a519115a0eabd79
Jamie <jas3465@gmail.com>
2018-12-11 16:47:00 -0500

    updating home page

---
+++ index2.php
@@ -0,0 +1,8 @@
+<script type='text/javascript' src='https://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js?ver=1.3.2'></script>
+<script type="text/javascript" src="js/jquery.jeditable.mini.js"></script>
+<script type="text/javascript" src="js/jsplumb-master/dist/js/jsplumb.min.js "></script>
+<script type="text/javascript" src="js/customPlumb.js"></script>
+<script type="text/javascript" src="js/custom.js"></script>
+<script type="text/javascript">
+    initialize();
+</script>
