9518e9a29282ebebd19522ebabdfc04ae719c7e1
Jamie <jas3465@gmail.com>
2018-12-11 23:46:12 -0500

    generic

--- js/custom.js
+++ js/custom.js
@@ -1,20 +1,34 @@
 var idx = 0;
-
-var colors = ['#F2326B','#FDC131','#23D2BE','#178ACD'];
-
 var newEntity = 0;
-
-function setColor(entityId) {
-  document.getElementById(entityId).style.backgroundColor = colors[++idx%4];
-};
 
 function initialize() {
 
 };
 
+var colors = ['#F2326B','#FDC131','#23D2BE','#178ACD'];
+function setColor(entityId) {
+  document.getElementById(entityId).style.backgroundColor = colors[++idx%4];
+};
+
+
+
+$(".types li").click(function () {
+
+  var entityId = "location" + newEntity++;
+
+  $("#editor")
+    .append("<div id='" + entityId + "'></div>");
+
+  $("#" + entityId).addClass("circle");
+
+  document.getElementById(entityId).style.backgroundColor = setColor(entityId);
+
+  jsPlumb.draggable(entityId);
+});
+
 $("#location").click(function () {
 
-  var entityId = "circle" + newEntity++;
+  var entityId = "location" + newEntity++;
 
   $("#editor")
     .append("<div id='" + entityId + "'></div>");
