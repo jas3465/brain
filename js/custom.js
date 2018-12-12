188ec7f10b9b793c8fe7dd929568edb143afb9ed
Jamie <jas3465@gmail.com>
2018-12-11 23:42:14 -0500

    remove tests

--- js/custom.js
+++ js/custom.js
@@ -2,20 +2,13 @@ var idx = 0;
 
 var colors = ['#F2326B','#FDC131','#23D2BE','#178ACD'];
 
-var newEntity = 3;
+var newEntity = 0;
 
 function setColor(entityId) {
   document.getElementById(entityId).style.backgroundColor = colors[++idx%4];
 };
 
 function initialize() {
-  $("#editor")
-    .append("<div id='circle'>test</div>");
-
-  $("#circle")
-    .addClass("circle");
-
-    document.getElementById("circle").style.backgroundColor = setColor("circle");
 
 };
 
@@ -24,7 +17,7 @@ $("#location").click(function () {
   var entityId = "circle" + newEntity++;
 
   $("#editor")
-    .append("<div id='" + entityId + "'>Location</div>");
+    .append("<div id='" + entityId + "'></div>");
 
   $("#" + entityId).addClass("circle");
 
