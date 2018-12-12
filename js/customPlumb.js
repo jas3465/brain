7f50860ca62dd8f242eb012e69b112a10c04bfa1
Jamie <jas3465@gmail.com>
2018-12-11 21:31:05 -0500

    remove defaults

--- js/customPlumb.js
+++ js/customPlumb.js
@@ -1,17 +1,6 @@
 jsPlumb.ready(function () {
 
-    var instance = jsPlumb.getInstance({
-        PaintStyle:{ 
-            strokeWidth:6, 
-            stroke:"#567567", 
-            outlineStroke:"black", 
-            outlineWidth:1 
-          },
-          Connector:[ "Bezier", { curviness: 30 } ],
-          Endpoint:[ "Dot", { radius:5 } ],
-          EndpointStyle : { fill: "#567567"  },
-          Anchor : [ 0.5, 0.5, 1, 1 ]
-    });
+    var instance = jsPlumb.getInstance();
 
     jsPlumb.draggable("circle");
     jsPlumb.draggable("circle2");
