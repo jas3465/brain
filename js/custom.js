e47ca30ff39ec265a64505aaf84b57541df5e501
Jamie <jas3465@gmail.com>
2018-12-12 00:11:52 -0500

    test

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,7 @@
 var idx = 0;
 var newEntity = 0;
+var x_pos = 60;
+var y_pos = 60;
 
 function initialize() {
 
@@ -17,11 +19,18 @@ $("#types li").click(function () {
   var entityId = "location" + newEntity++;
   var type = $(this).attr('id');
   $("#editor")
-    .append("<div id='" + entityId + "'>" + type + "</div>");
+    .append("<div id='" + entityId + "'><span>" + type + "</span></div>");
 
-  $("#" + entityId).addClass("circle");
+  $("#" + entityId).addClass("entity");
 
-  document.getElementById(entityId).style.backgroundColor = setColor(entityId);
+  var d = document.getElementById(entityId);
+
+  d.style.backgroundColor = setColor(entityId);
+  d.style.left = x_pos+'px';
+  d.style.top = y_pos+'px';
+
+  x_pos = x_pos + 100;
+  v_pos = v_pos + 100;  
 
   jsPlumb.draggable(entityId);
 });
