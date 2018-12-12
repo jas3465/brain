a12fefd562ce711f0c5f15b286f28db0c77307e8
Jamie <jas3465@gmail.com>
2018-12-12 00:58:40 -0500

    checks

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,7 @@
 var idx = 0;
 var newEntity = 0;
-var x_pos = 60;
-var y_pos = 60;
+var x_pos = 300;
+var y_pos = 250;
 
 function initialize() {
 
@@ -14,10 +14,15 @@ function setColor(entityId) {
 
 
 
-$("#types li").click(function () {
+$("#types li").one("click", function () {
 
-  var entityId = "location" + newEntity++;
   var type = $(this).attr('id');
+  if (type == "location" || type == "restaurant" || type == "atm" || type == "healthcareFacility" || type == "healthcareProfessional" || type == "event"){
+    var entityId = type + newEntity++;
+    $(this).append("<img src='check.png' class='check'/>");   }
+  else
+    var entityId = "custom" + newEntity++;
+  
   $("#editor")
     .append("<div id='" + entityId + "'><span>" + type + "</span></div>");
 
@@ -29,8 +34,8 @@ $("#types li").click(function () {
   d.style.left = x_pos+'px';
   d.style.top = y_pos+'px';
 
-  x_pos = x_pos + 100;
-  v_pos = v_pos + 100;  
+  x_pos = x_pos + 200;
+  y_pos = y_pos + 200;  
 
   jsPlumb.draggable(entityId);
 });
